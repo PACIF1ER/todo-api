a48be360f4570ec751a5d8b13662d5b09022227e
PACIF1ER <komaygorodsky@i.ua>
2017-06-26 20:55:16 +0300

    deleting usless controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
-
-  root to: "static_pages#index"
-  resources :tasks, only: [:index, :create] do
-    post :complete, to: "tasks#complete"
-  end
-  resources :users, only: [:new, :create]
-  resources :sessions, only: [:new, :create, :destroy]
-
-  # API
+# API
   namespace :api do
     namespace :v1 do
       resources :tasks, only: [:index, :create, :destroy] do
